d6eaf03d0d15dc7634109103dfa0827267d73a44
Justin Kumpe <jakumpe@kumpes.com>
2024-03-11 18:52:58 -0500

    updated setup

--- setup.py
+++ setup.py
@@ -1,15 +1,16 @@
 """Install missing modules"""
+
 import pip
 
 
-def import_or_install(module, package = None):
+def import_or_install(module, package=None):
     """install module if unable to import"""
     if package is None:
         package = module
     try:
         __import__(module)
     except ImportError:
-        pip.main(['install', package])
+        pip.main(["install", package])
 
 
 import_or_install("bs4", "beautifulsoup4")
@@ -17,4 +18,4 @@ import_or_install("imap-tools")
 import_or_install("mysql.connector")
 import_or_install("pymysql")
 import_or_install("requests")
-import_or_install("infisical-api")
+import_or_install("infisical_api", "infisical-api")
