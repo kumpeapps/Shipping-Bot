dbf12da65f872e7eb30f1bbd2be8eea045cbf12b
Justin Kumpe <jakumpe@kumpes.com>
2024-07-27 17:55:43 -0500

    temp disable pitneyship

--- pitney_ship.py
+++ pitney_ship.py
@@ -4,21 +4,21 @@ from bs4 import BeautifulSoup
 
 def scrape_pitneyship(file):
     """Scrape and parse PitneyShip email"""
-    soup = BeautifulSoup(file, 'html.parser')
-    for br_tag in soup.find_all("br"):
-        br_tag.replace_with("\n")
-    th_data = soup.find_all('th')
-    tracking_id = th_data[18].text.splitlines()[2].replace(" =20", "")
-    estimated_delivery = th_data[20].text.replace("  ", "").splitlines()[1]
-    courier = th_data[22].text.replace(" ", "").splitlines()[1]
-    address_block = th_data[26].text.replace("  ", "").replace("MO=", "")
-    customer = address_block.splitlines()[1]
-    street_address = address_block.splitlines()[2]
+    # soup = BeautifulSoup(file, 'html.parser')
+    # for br_tag in soup.find_all("br"):
+    #     br_tag.replace_with("\n")
+    # th_data = soup.find_all('th')
+    # tracking_id = th_data[18].text.splitlines()[2].replace(" =20", "")
+    # estimated_delivery = th_data[20].text.replace("  ", "").splitlines()[1]
+    # courier = th_data[22].text.replace(" ", "").splitlines()[1]
+    # address_block = th_data[26].text.replace("  ", "").replace("MO=", "")
+    # customer = address_block.splitlines()[1]
+    # street_address = address_block.splitlines()[2]
     return {
-        "tracking_id": tracking_id,
-        "estimated_delivery": estimated_delivery,
-        "courier": courier,
-        "customer": customer,
-        "street_address": street_address,
+        # "tracking_id": tracking_id,
+        # "estimated_delivery": estimated_delivery,
+        # "courier": courier,
+        # "customer": customer,
+        # "street_address": street_address,
         "system": "ACC"
         }
